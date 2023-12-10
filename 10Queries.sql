4d587176e0ab5cd47bc364741dfbab3603493e6a
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2023-12-10 11:00:14 -0500

    adding edwins 4 queries

--- 10Queries.sql
+++ 10Queries.sql
@@ -1,4 +1,60 @@
--- 10 queries 
+/* =============================================
+-- Author:		Edwin Wray
+-- Create date: 12/10/23
+-- Proposition:	Which instructors are teaching in classes in multiple departments?
+-- =============================================*/
+
+SELECT DI.InstructorID, I.FirstName, I.LastName
+FROM Personnel.DepartmentInstructor AS DI
+    INNER JOIN Personnel.Instructor AS I 
+        ON DI.InstructorID = I.InstructorID
+GROUP BY DI.InstructorID,  I.FirstName, I.LastName
+HAVING COUNT(DISTINCT DI.DepartmentID) > 1
+ORDER BY DI.InstructorID;
+
+
+/* =============================================
+-- Author:		Edwin Wray
+-- Create date: 12/10/23
+-- Proposition:	How many instructors are in each department?
+-- =============================================*/
+
+SELECT D.DepartmentID, D.DepartmentName, COUNT(DI.InstructorID) AS NumberOfInstructors
+FROM Academic.Department AS D
+    LEFT JOIN Personnel.DepartmentInstructor AS DI 
+        ON D.DepartmentID = DI.DepartmentID
+GROUP BY D.DepartmentID, D.DepartmentName
+ORDER BY D.DepartmentID;
+
+    
+/* =============================================
+-- Author:		Edwin Wray
+-- Create date: 12/10/23
+-- Proposition:	How many classes are being taught this semester?
+                Group by course and aggregating total enrollment, 
+                total class limit and the percentage of Enrollment.
+-- =============================================*/
+
+SELECT DISTINCT
+    C.CourseID, C.CourseName
+    , SUM(DISTINCT CS.ClassID) AS TotalClasses
+    , E.CurrentEnrollment AS CurrentEnrollment
+    , E.MaxEnrollmentLimit AS MaxEnrollmentLimit
+FROM
+    ClassManagement.Class AS CS
+        JOIN Academic.Course AS C 
+            ON CS.CourseID = C.CourseID
+        LEFT JOIN Academic.Section AS S 
+            ON C.CourseID = S.CourseId
+        LEFT JOIN Enrollment.EnrollmentDetail AS E 
+            ON S.SectionID = E.SectionID
+GROUP BY
+    C.CourseID, C.CourseName, E.CurrentEnrollment, E.MaxEnrollmentLimit
+ORDER BY
+    C.CourseID;
+
+
+------------------------------ 10 additional queries ----------------------------------
 
 /* =============================================
 -- Author:		Aleksandra Georgievska
@@ -141,6 +197,25 @@ FROM Academic.Course AS C
     INNER JOIN Enrollment.EnrollmentDetail AS E
         ON S.SectionID = E.SectionID
 WHERE E.CurrentEnrollment > E.MaxEnrollmentLimit
+
+/* =============================================
+-- Author:		Edwin Wray
+-- Create date: 12/10/23
+-- Proposition:	What courses are offered by a specific department? 
+                Include the courses names and credit hours.
+-- =============================================*/
+
+DECLARE @DepartmentID INT;
+-- Set the value of the specific department
+SET @DepartmentID = 1;
+
+SELECT @DepartmentID AS DepartmentID, D.DepartmentName
+    , C.CourseId, C.CourseName, C.CreditHours
+FROM Academic.Department AS D
+    INNER JOIN Academic.Course AS C
+        ON D.DepartmentID = C.DepartmentID
+WHERE D.DepartmentID = @DepartmentID
+
 
 
 
